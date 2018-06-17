1fd7ace0a29d64a5e360294b15624a246104ba05
Touhidul Alam <alamtl@ims.uni-stuttgart.de>
2018-06-18 00:26:04 +0200

    updated CKY

--- CYK.py
+++ CYK.py
@@ -1,7 +1,8 @@
 import sys
 import string
 import re
-import itertools
+#import itertools
+import nltk
 
 class ConvertCNF():
 	def __init__(self, rules, startSymbol):
@@ -36,11 +37,10 @@ class ConvertCNF():
 							self.rules[key2].remove('ε')
 						else:
 							for item in value2:
-								new_prod = self.create_prod_combinations(item, key, item.count(key))
+								new_prod = self.createProdCombinations(item, key, item.count(key))
 								self.rules[key2] = self.rules[key2] + new_prod
 				self.rules[key] = list(set(self.rules[key]))
 				self.rules[key].remove('ε')
-				#print(self.rules)
 
 		for key, value in self.rules.items():
 			for i in range(len(value)):
@@ -50,23 +50,23 @@ class ConvertCNF():
 			value = set(value)
 			value = list(value)
 			self.rules[key] = value
-		print("After Epsilon Elimination: ")
-		print(self.rules)
+		#print("After Epsilon Elimination: ")
+		#print(self.rules)
 
-	def create_prod_combinations(self, prod, nt, count):
+	def createProdCombinations(self, item, nonterminal, count):
 	        numset = 1 << count
 	        new_prods = []
 
 	        for i in range(numset):
 	            nth_nt = 0
 	            new_prod = ''
-	            for s in prod:
-	                if s == nt:
-	                    if i & (1 << nth_nt):
-	                        new_prod = new_prod+s
-	                    nth_nt += 1
-	                else:
-	                    new_prod = new_prod+s
+	            for s in item:
+	            	if s == nonterminal:
+	            		if i & (1 << nth_nt):
+	            			new_prod = new_prod+s
+	            		nth_nt += 1
+	            	else:
+	            		new_prod = new_prod+s
 	            new_prods.append(new_prod)
 	        return new_prods
 
@@ -78,10 +78,8 @@ class ConvertCNF():
 					if key in value2 and key==key2:
 						self.rules[key].remove(key)
 					if key in value2:
-						#print(key)
 						self.rules[key2] = self.rules[key2] + self.rules[key]
 						self.rules[key2].remove(key)
-						#for removing one unit, remove from other rules too
 					for c in value2:
 						if key in c:
 							if key==c and flag!=2:
@@ -94,8 +92,8 @@ class ConvertCNF():
 				self.rules.pop(key, None)
 
 
-		print("After eliminate variable unit")
-		print(self.rules)
+		#print("After eliminate variable unit")
+		#print(self.rules)
 
 	def replaceLongProd(self):
 		for key, value in self.rules.items():
@@ -112,16 +110,13 @@ class ConvertCNF():
 								if len(value2[j].split(" "))!=2:
 									self.rules[key][i]=self.rules[key][i].replace(value2[j], key2)
 								#pass
-		#print(self.rules)
 
 		for key, value in self.rules.copy().items():
 			for i in range(len(value)):
 				if value[i] in self.rules:
 					if all( tkey.startswith("'") for tkey in self.rules[value[i]]):
-						#probably need to work for index 0
 						self.rules[key][i] = self.rules[key][i].replace(value[i],self.rules[value[i]][0] )
 
-		#print(self.rules)
 
 		for key, value in self.rules.copy().items():
 			for i in range(len(value)):
@@ -133,8 +128,6 @@ class ConvertCNF():
 					
 					self.rules[self.temp_list[self.rules[key][i].split(" ")[0]+" "+self.rules[key][i].split(" ")[1]]] = [self.rules[key][i].split(" ")[0]+" "+self.rules[key][i].split(" ")[1]]
 					self.rules[key][i] = self.rules[key][i].replace(self.rules[key][i].split(" ")[0]+" "+self.rules[key][i].split(" ")[1], self.temp_list[self.rules[key][i].split(" ")[0]+" "+self.rules[key][i].split(" ")[1]])
-
-		#print(self.rules)
 
 	def getNewNTSymbol(self):
 		nt = list(string.ascii_uppercase)
@@ -154,7 +147,6 @@ class ConvertCNF():
 					match = re.findall(r'\'+\w+\'', value[i])
 					for i in match:
 						self.terminal[i] = key
-		#Need to check here with real example
 		for item, value in self.terminal.items():
 			dict_new_nt = item.upper()
 			dict_new_nt = dict_new_nt.replace("'","")
@@ -162,29 +154,36 @@ class ConvertCNF():
 				dict_new_nt = self.getNewNTSymbol()
 			self.rules[dict_new_nt] = [item]
 			self.terminal[item] = dict_new_nt
-		print("After move terminal to unit:")
-		print(self.rules)
+		#print("After move terminal to unit:")
+		#print(self.rules)
 
 class CYK():
 	def __init__(self, path, string_path):
+		self.temp_table = []
 		self.path = path
 		self.string_path = string_path
 		self.startSymbol = ''
 		self.rules = {}
+		self.words = []
+		print("\nInput Grammar:")
 		self.readGrammar()
 		self.readString()
 		self.converted= ConvertCNF(self.rules, self.startSymbol)
-		#self.word = ''
-		print("Final Grammar:")
-		print(self.converted.rules)
-		self.readOutput()
-		self.parser()
+		i=1
+		for item in self.words:
+			self.word = item
+			print("\n"+str(i)+"----------------------------")
+			print("\nFinal Grammar:")
+			i+=1
+			self.readOutput()
+			print(self.word)
+			self.parser()
 
 	def readGrammar(self):
 		f = open(self.path)
 		self.startSymbol = f.readline().rstrip()
 		for content in f:
-			print(content)
+			print(content.strip("\n"))
 			content = content.rstrip()
 			rule = content.split(" -> ")
 
@@ -192,18 +191,31 @@ class CYK():
 				self.rules[rule[0]] = rule[1].split(" | ")
 			else:
 				self.rules[rule[0]] += (rule[1].split(" | "))
-		print(self.rules)
 
 	def readString(self):
 		f = open(self.string_path)
 		for content in f:
-			self.word = content
+			content = content.rstrip()
+			self.words.append(content)
 
 	def readOutput(self):
+		str1=''
 		for key, value in self.converted.rules.items():
-			print( key + " -> "+ ' | '.join(map(str, value)))
+			str1 += str(key) + " -> "+ ' | '.join(map(str, value))+'\n'
+			#print( key + " -> "+ ' | '.join(map(str, value))
+		print(str1)
+
+	
+		groucho_grammar = nltk.CFG.fromstring(str1)
+		sent = self.word.split(" ")
+		parser = nltk.ChartParser(groucho_grammar)
+		print("\nParse Tree:")
+		for tree in parser.parse(sent):
+			print(tree)
+		
 
 	def parser(self):
+		self.copy_table = []
 		wordList = self.word.split(" ")
 		length = len(wordList)
 		#print(length)
@@ -214,15 +226,17 @@ class CYK():
 				for item in value:
 					if item== "'"+word+"'":
 						self.parse_table[0][i].append(key)
+						self.copy_table.append((key, item))
 
 		i=0
-		for words_to_consider in range(2, length + 1):
-			for starting_cell in range(0, length - words_to_consider + 1):
-				for left_size in range(1, words_to_consider):
-					right_size = words_to_consider - left_size
+		for possible_words in range(2, length + 1):
+			for current_cell in range(0, length - possible_words + 1):
+				for left_side in range(1, possible_words):
+					right_side = possible_words - left_side
 
-					left_cell = self.parse_table[left_size - 1][starting_cell]
-					right_cell = self.parse_table[right_size - 1][starting_cell + left_size]
+					left_cell = self.parse_table[left_side - 1][current_cell]
+					right_cell = self.parse_table[right_side - 1][current_cell + left_side]
+
 					left_flag=0
 					right_flag=0
 					for key, value in self.converted.rules.items():
@@ -235,22 +249,78 @@ class CYK():
 										if right==subitem[1]:
 											right_flag=1
 											if left_flag==1 and right_flag==1:
-												self.parse_table[words_to_consider - 1][starting_cell].append(key)
-												left_flag=0
-												right_flag=0
-		#print(self.parse_table)
-		print("\n")
+												print(key)
+												self.parse_table[possible_words - 1][current_cell].append(key)
+												self.copy_table.append((key, [subitem[0], subitem[1]], (left_side - 1, current_cell), (right_side - 1, current_cell + left_side), (possible_words - 1, current_cell) ))
+										left_flag=0
+										right_flag=0
+		#self.copy_table = self.parse_table
+
+		for item in self.copy_table:
+			if item[0] != self.startSymbol:
+				if all(item[0] not in item2[1] for item2 in self.copy_table):
+					self.copy_table.remove(item)
+		
+		print("\nCKY Chart")
 		for item in reversed(self.parse_table):
 			print(' __ '.join(map(str, item)))
 		print(" ____ ".join(map(str, self.word.split(" "))))
-
+		
+		
 		if self.parse_table[-1][0]:
-			if self.parse_table[-1][0][0] == self.startSymbol:
-				print("Accepted String")
+			if self.startSymbol in self.parse_table[-1][0]:
+				#print("\n")
+				self.copy_table = self.copy_table[::-1]
+				#print("\n")
+				self.copy_table_reversed = self.copy_table[::-1]
+				print("----")
+				print(self.copy_table_reversed)
+				print("----")
+				a = self.generate_tree(self.copy_table[0])
+				print("Parse Tree:")
+				print(a)
+				#for item in self.copy_table:
+				#	print(item)
+				print("\nAccepted String")
 		else:
-			print("Rejected String")
-		#grammar 3/5 to look
+			print("\nRejected String")
 
+	def generate_tree(self, node):
+		#print(node)
+		if isinstance(self.find(self.copy_table, node)[1], (list,)):
+			item = self.find(self.copy_table, node)[1]
+			item_x = self.find(self.copy_table, node)[2]
+			item_y = self.find(self.copy_table, node)[3]
+
+			if item_x[0]==0:
+				x=self.copy_table_reversed[item_x[1]]
+			else:
+				x=self.find_tuple(self.copy_table, item_x)
+
+			if item_y[0]==0:
+				y=self.copy_table_reversed[item_y[1]]
+			else:
+				y=self.find_tuple(self.copy_table, item_y)
+
+			return (node[0], self.generate_tree(x), self.generate_tree(y))
+		else:
+			return (node[0],  self.find(self.copy_table, node)[1])
+
+	def find_tuple(self, list_of_tuples, value):
+		val = []
+		for x in list_of_tuples:
+			if x[4]==value:
+				val = x
+				break
+		return val
+
+	def find(self, list_of_tuples, value):
+		val = []
+		for x in list_of_tuples:
+			if x==value:
+				val = x
+				break
+		return val
 
 if __name__ == "__main__":
     CYK(sys.argv[1], sys.argv[2])
--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# Parsing
+CYK Parsing Implementation
+
+
+Programming Language- Python 3.6.1
+
+
+Overview:
+
+
+CKY.py
+* Class ConverCNF()
+   * startSymbolAdd() - function adds a new start symbol if existing symbol already on the right hand side of the rules
+   * eliminateEpsilon() - if rule contains any ‘ε’ in the right hand side, it removes and restructure the grammar accordingly
+   * eliminateVariableUnit() - This function removes single production non-terminal from the right hand side
+   * moveTerminalToUnits() - this function add unit non-terminal for terminals
+   * replaceLongProd() - replace any non-terminal production more than 2 are adjusted in the form of A -> B C
+* Class CYK()
+   * readGrammar() - read input grammar, add the rules as dictionary, where key goes the left-hand side rule and value contains right hand-side rules in a list
+   * readString() - read input string from the text files
+   * readOutput() - print the modified grammar & CKY table
+   * parser() - CKY parser algorithm
+
+
+
+
+Dependencies:
+
+
+re - should be included with python by default
+NLTK-
+* Pip3 install nltk
+
+
+Usage:
+
+
+* On terminal where the files exist, write command
+	Python3 CYK.py <grammar_file_.txt> <string_file_.txt>
+* Sample grammar file as included
+   * First line contains the start symbol
+   * Epsilon defines with ‘ε’ symbol
+   * Terminals are separated by single-quote (‘’) e.g. ‘John’, ‘man’
+   * Multiple terminals and non-terminals are separated by whitespace
+* Sample string file as included- each line contains separate string
